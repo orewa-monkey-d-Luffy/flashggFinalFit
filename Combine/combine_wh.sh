25f1da324f54df766f8eafdf4275d42a4cbd00a2
bmjoshi <joshi.bm163@gmail.com>
2024-02-01 00:52:13 +0100

    updates

--- Combine/combine_wh.sh
+++ Combine/combine_wh.sh
@@ -11,14 +11,35 @@ fggff_ws_twoHiggs_model_opts="-m 125.0 -P  HiggsAnalysis.CombinedLimit.HiggsJPC:
 fggff_combine_twoHiggs_model_opts="--algo=grid -M MultiDimFit --robustFit=1 --setRobustFitAlgo=Minuit2,Migrad --X-rtd FITTER_NEW_CROSSING_ALGO --setRobustFitTolerance=0.1 --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND --cminFallbackAlgo Minuit2,0:1. --cminDefaultMinimizerStrategy 0 --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --saveSpecifiedNuis all --saveInactivePOI 1 --freezeParameters MH --setParameterRanges r=0.5,4.0 -m 125.00"
 
 
+########################################################################################
+#  Run without floating parameters
+########################################################################################
 
-dirext="${tag}_VHLEP_combined_${ext}"
-output_ws="vh_lep_workspace_${dirext}.root"
-datacard="Models/datacards/Datacard_${tag}_combined_${ext}_ANOM_STXS_VH_LEP_Int.txt"
+dirext="${tag}_combined_${ext}"
+output_ws="wh_workspace_${dirext}.root"
+datacard="Models/datacards/Datacard_${tag}_combined_${ext}_ANOM_STXS_WH_Int.txt"
 text2workspace.py $datacard -o $output_ws $fggff_ws_int_model_opts
 for poi in CMS_zz4l_fai1 muV; do
-      prof_name="_profile1D_syst_CMS_${poi}_VHLeptonic_${dirext}"
-      combineTool.py -d $output_ws $fggff_combine_int_model_opts -n $prof_name
-      plot1DScan.py --POI $poi "higgsCombine$prof_name.MultiDimFit.mH125.root" -o "${poi}_VHLeptonic_${dirext}" --main-label STXS-ANOM_$poi --y-max 30
-      #cp "./${poi}_VHLeptonic_${dirext}.pdf" /eos/user/b/bjoshi/www/VHAnomalous/13_11_2023/
+     prof_name="_profile1D_syst_CMS_${poi}_WHLeptonic_${dirext}"
+     combineTool.py -d $output_ws $fggff_combine_int_model_opts -n $prof_name
+     plot1DScan.py --POI $poi "higgsCombine$prof_name.MultiDimFit.mH125.root" -o "${poi}_WHLeptonic_${dirext}" --main-label STXS-ANOM_$poi --y-max 30
+     #cp "./${poi}_WHLeptonic_${dirext}.pdf" /eos/user/b/bjoshi/www/VHAnomalous/13_11_2023/
 done
+
+
+########################################################################################
+#  Run with floating parameters
+########################################################################################
+
+# dirext="${tag}_combined_${ext}"
+# output_ws="wh_float_workspace_${dirext}.root"
+# datacard="Models/datacards/Datacard_${tag}_combined_${ext}_ANOM_STXS_WH_Int.txt"
+# echo text2workspace.py $datacard -o $output_ws $fggff_ws_int_model_opts
+# for poi in CMS_zz4l_fai1 muV; do
+#      prof_name="_profile1D_syst_CMS_${poi}_WHLeptonic_float_${dirext}"
+#      echo combineTool.py -d $output_ws $fggff_combine_int_model_opts_float -n $prof_name
+#      echo plot1DScan.py --POI $poi "higgsCombine$prof_name.MultiDimFit.mH125.root" -o "${poi}_WHLeptonic_float_${dirext}" --main-label STXS-ANOM_$poi --y-max 30
+#      #cp "./${poi}_WHLeptonic_float_${dirext}.pdf" /eos/user/b/bjoshi/www/VHAnomalous/13_11_2023/
+# done
+
+
